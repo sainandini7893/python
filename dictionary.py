88856a076de88c05b42faac6a8e3f13133fbc332
sainandini7893 <gujjalasainandini@gmail.com>
2025-08-08 17:48:15 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+#creating a dictionary
+#my_dict = {'name':'nandu','age':20,'house':'findoor'}
+#print(my_dict)
+
+#Accessing a dictionary
+my_dict = {'name':'nandu','age':20,'branch':'cse'}
+name_value = my_dict['name']
+age_value = my_dict['age']
+branch_value = my_dict['branch']
